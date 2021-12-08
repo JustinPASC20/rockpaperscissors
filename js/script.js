884b2f7ae4dab362a5f027d071065733b9adf6b3
Glitch (rps-real) <none>
2021-12-08 23:18:42 +0000

    🐔🚠 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
+let computerChoice =Math.ceil(Math.random()*3);
 
+
+$(".computerChoice").text(computerChoice);
+
+$(".play").click(function(){
+  let userInput = $(".input").val();
+  $(".userChoice").text(userInput);
+})
+
+
+/*
+if(computerChoice === 1){
+computerChoice = "rock";
+$(".computerChoice").text(computerChoice);}
+
+else if(computerChoice ===2){
+computerChoice = "scissors";
+$(".computerChoice").text(computerChoice);
+}
+
+else if(computerChoice ===3){
+computerChoice = "paper";
+$(".computerChoice").text(computerChoice);
+}
+*/
+
+//compound conditional statements
+/*
+if((userInput === "rock" && computerChoice === 2 || userInput ==="scissors" && computerChoice === 3 || userInput === "paper" && computerChoice === 1){
+
+}
+*/
